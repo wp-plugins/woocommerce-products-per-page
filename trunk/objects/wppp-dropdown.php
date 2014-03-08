85abe4eae1afa5fd71b758470bcd0e2781cb8956
sormano <sormano@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-08 15:43:10 +0000

    Tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-products-per-page/tags/1.0.1@871882 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/objects/wppp-dropdown.php
@@ -0,0 +1,59 @@
+<?php
+
+class wppp_dropdown extends woocommerce_products_per_page {
+	
+	public $productPerPage;
+	
+	public function __construct( $productPerPage = null ) {
+		
+		parent::__construct();
+		
+		$this->productsPerPage = $this->wppp_prep_ppp( $productPerPage );
+		
+		if( !$productPerPage )
+			$this->productsPerPage = $this->wppp_prep_ppp( apply_filters( "wppp_product_per_page", $this->options["productsPerPage"] ) );
+			
+		$this->wppp_create_object();
+		
+	}
+	
+	
+	public function wppp_create_object() {
+
+		?>
+		<form method="post" class="form-wppp-select products-per-page">
+			<?php
+			do_action( "wppp_before_dropdown" );
+			?>
+			<select name="wppp_ppp" onchange="this.form.submit()" class="select wppp-select">
+			
+				<?php
+				global $woocommerce;
+				foreach( $this->productsPerPage as $key => $value ) :
+					$selectedMatch = isset( $_POST["wppp_ppp"] ) ? $_POST["wppp_ppp"] : $woocommerce->session->get( "products_per_page" );
+					?>
+					<option value="<?php echo $value; ?>" <?php selected( $value, $selectedMatch ); ?>>
+						<?php printf( __( "%s products per page", "wppp" ), $value==-1?__( "All", "wppp" ) : $value ) ?>
+					</option>
+					<?php
+				endforeach;
+				?>
+			</select>
+			<?php
+			do_action( "wppp_after_dropdown" );
+			?>
+		</form>
+		<?php
+		
+	}
+	
+	
+	public function wppp_prep_ppp( $productPerPage ) {
+
+		return explode( " ", $productPerPage );
+		
+	}
+	
+}
+
+?>
